3d185bfc1672fc176ff8101903d3ad340ecc73f2
viktorlyt <viktorlyt@gmail.com>
2023-05-24 00:04:29 +0300

    done

--- src/ifElse.test.js
+++ src/ifElse.test.js
@@ -1,11 +1,27 @@
 'use strict';
 
 describe('ifElse', () => {
-  // const { ifElse } = require('./ifElse');
+  const { ifElse } = require('./ifElse');
 
-  it('should ', () => {
+  it('should call first if condition is true', () => {
+    const condition = jest.fn(() => true);
+    const first = jest.fn();
+    const second = jest.fn();
 
+    ifElse(condition, first, second);
+
+    expect(first).toHaveBeenCalled();
+    expect(second).not.toHaveBeenCalled();
   });
 
-  // write tests here
+  it('should call first if condition is false', () => {
+    const condition = jest.fn(() => false);
+    const first = jest.fn();
+    const second = jest.fn();
+
+    ifElse(condition, first, second);
+
+    expect(first).not.toHaveBeenCalled();
+    expect(second).toHaveBeenCalled();
+  });
 });
